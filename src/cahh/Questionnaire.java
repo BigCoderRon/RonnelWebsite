a9d44406ca4bda9d073886c52f2226143eef0ffc
Ronnel Salgado <Ronnel@192.168.0.19>
2022-11-23 21:36:25 +0000

    This is the questionnaire class, Questions that we will ask the user will be stored here.

--- src/cahh/Questionnaire.java
+++ src/cahh/Questionnaire.java
@@ -9,5 +9,16 @@ package cahh;
  * @author Ronnel
  */
 public class Questionnaire {
+    public String question1 = "Where do you live?";
+
+    public String getQuestion1() {
+        return question1;
+    }
+
+    public void setQuestion1(String question1) {
+        this.question1 = question1;
+    }
     
+            
+       
 }
