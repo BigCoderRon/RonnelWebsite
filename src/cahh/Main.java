fd78c8a1534a565f1f643b8df9dd7631682182a7
Ronnel Salgado <Ronnel@192.168.0.19>
2022-11-23 21:25:28 +0000

    new main method

--- src/cahh/Main.java
+++ src/cahh/Main.java
@@ -8,14 +8,14 @@ package cahh;
  *
  * @author Ronnel
  */
-public class 
+public class Main {
 
-   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
